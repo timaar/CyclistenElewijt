858058e21c302df38bbba3eab7cb5ac250bbbd48
Tim Van Meerbeeck <timaar@gmail.com>
2024-11-21 14:17:05 +0100

    feat() Added not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {RittenComponent} from './ritten/ritten.component';
 import {HomeComponent} from './home/home.component';
+import {NotFoundComponentComponent} from './not-found-component/not-found-component.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'ritten',
     component: RittenComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponentComponent
   }
 ];
