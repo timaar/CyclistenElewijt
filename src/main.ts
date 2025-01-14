2cbd937d3668e887ea32bec8934dc54f43410c35
Tim Van Meerbeeck <timaar@gmail.com>
2025-01-14 19:16:44 +0100

    Cors issue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-if ('serviceWorker' in navigator && environment.production) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/coi-serviceworker.min.js')
     .then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
