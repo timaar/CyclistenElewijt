24f84da5befd0e3a39967ef0914e20148b879035
Tim Van Meerbeeck <timaar@gmail.com>
2025-01-14 19:40:55 +0100

    revert Cors issue

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/coi-serviceworker.min.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-}
